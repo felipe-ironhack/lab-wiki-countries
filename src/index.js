54aa574846a5a94897a545c01d1f617294f41382
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2022-10-20 11:06:09 +0200

    update to React v18

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
 import { BrowserRouter as Router } from 'react-router-dom'
+
 import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <Router>
     <App />
-  </Router>,
-  document.getElementById('root')
+  </Router>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
