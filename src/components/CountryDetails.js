a23ba3b50b13bfda844aa3ab4e0dde802576696e
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2023-01-19 14:38:21 +0100

    different solutions on details and some update notes

--- src/components/CountryDetails.js
+++ src/components/CountryDetails.js
@@ -4,22 +4,35 @@ import { useParams } from 'react-router-dom';
 const CountryDetails = ({countries}) => {
   const { id } = useParams();
 
-  // const [country, setCountry] = useState([])
+  const [country, setCountry] = useState([])
 
-  // const getCountry = () => {
-  //   const copy = [...countries]
-  //   const filtered = copy.filter(country => country.alpha3Code === id)
-  //   setCountry(filtered[0])
-  // }
+  const getCountry = () => {
+    const copy = [...countries]
+    const filtered = copy.filter(country => country.alpha3Code === id)
+    setCountry(filtered[0])
+  }
   
-  // useEffect(() => {
-    //   getCountry()
-    // }, [id])
+  useEffect(() => {
+      getCountry()
+    }, [id])
     
   // const copy = [...countries]
-  const country = countries.filter(country => country.alpha3Code === id)[0]
+  // const country = countries.filter(country => country.alpha3Code === id)[0]
 
-  return <div> {country.name?.common} </div>;
-};
+  return (
+  <div> 
+    {country.name && 
+      <>
+      
+        <h3> {country.name.common} </h3>
+        <p> {country.alpha2Code} </p>
+        
+    
+    
+    
+      </>
+    } 
+  </div>
+)};
 
 export default CountryDetails;
